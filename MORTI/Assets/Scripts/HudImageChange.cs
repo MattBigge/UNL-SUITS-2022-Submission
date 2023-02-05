0146d4cc7f0a8074e6147bcc083dc43c8af77ac1
Peyton Comer <pcomer2@huskers.unl.edu>
2023-02-04 19:51:54 -0600

    Added Directional illustrator and placeholder HUD.
    Outlined the pinpoint following mechanism and outlined some potential hud positions.

--- MORTI/Assets/Scripts/HudImageChange.cs
+++ MORTI/Assets/Scripts/HudImageChange.cs
@@ -6,7 +6,10 @@ using System.Linq;
 
 public class HudImageChange : MonoBehaviour
 {
-    // public Image HudTester;
+    /*
+     * The basic idea is to assign sprites given to us by Lindsey in unity to the image slots here. Once we have this we can then take inputs from the telemtry stream and allow the
+     * data to dictate what the hud looks like depending on what is needed.
+    */
     public Image oldImage;
     public Sprite image0;
     public Sprite image1;
@@ -17,11 +20,10 @@ public class HudImageChange : MonoBehaviour
     public int batteryLevel = 0;
     public int lastBatteryLevel = 0;
     public GameObject testingHudCanvas;
-    // Start is called before the first frame update
+// Start is called before the first frame update
     public void Start()
     {
-        
-        
+//adds the images to the list of sprites, subject to change depending on how many sprites are needed.
         batterySprites.Add(image0);
         batterySprites.Add(image1);
         batterySprites.Add(image2);
@@ -29,17 +31,18 @@ public class HudImageChange : MonoBehaviour
         batterySprites.Add(image4);     
 
     }
-    public void batteryLevelChanger(int changeValue)
-    {
-        batteryLevel = changeValue;
-    }
+    //public void batteryLevelChanger(int changeValue)
+    
+        //batteryLevel = changeValue;
+
     private void ImageChange(Sprite changeTo)
     {
         //tempImage = oldImage.sprite;
         oldImage.sprite = changeTo;
         //newImage = tempImage;
     }
-
+    
+//this is temporary and is a testing measure to see that the hud image would change. Currently changes with frame updates, will change with data updates in the future.
     public void Update()
     {
         ImageChange(batterySprites.ElementAt(batteryLevel));
